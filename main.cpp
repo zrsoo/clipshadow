b1ecc8d1fab359c86a6b7ec61299a7e313eaf55a
zrsoo <zneamtiu@gmail.com>
2025-05-25 19:50:24 +0300

    Clipboard extraction working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#include <windows.h>
+#include <string>
+#include <thread>
+
+std::string GetClipboardText()
+{
+    if(!OpenClipboard(nullptr))
+    {
+        return "Failed. Clipboard is not open.";
+    }
+
+    HANDLE hData = GetClipboardData(CF_TEXT);
+
+    char* textArray = static_cast<char*>(GlobalLock(hData));
+    if(textArray == nullptr)
+    {
+        CloseClipboard();
+        return "Failed. Cannot lock clipboard memory location.";
+    }
+
+    std::string text(textArray);
+
+    GlobalUnlock(hData);
+    CloseClipboard();
+
+    return text;
+}
+
 
 int main() {
-    int a = 5;
-    int b = 3;
-    int c = a + b;  // Set breakpoint here
+    std::cout << "[*] Clipboard Interceptor Started\n";
+    std::string lastClipboard = "";
 
-    std::cout << c << std::endl;
+    while(true)
+    {
+        std::string currentClipboard = GetClipboardText();
+
+        if(!currentClipboard.empty() && currentClipboard != lastClipboard) {
+            std::cout << "[*] New Clipboard Content:\n\n" << currentClipboard << "\n\n";
+            lastClipboard = currentClipboard;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+
     return 0;
 }
